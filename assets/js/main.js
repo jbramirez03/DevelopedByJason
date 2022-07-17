0ddd9cf97b5f3323a20e9e8e2b0e6091c2ea0485
temptado <jason1287712@gmail.com>
2022-07-17 17:43:10 -0400

    added scroll header script to add class to header when site is scrolled down

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,44 @@
+
+function scrollHeader() {
+    const header = document.getElementById('header');
+
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+};
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(l => l.addEventListener('click', linkAction));
+
+
+
 const accordionItems = document.querySelectorAll('.technologies__item');
 
 const toggleItem = (item) => {
